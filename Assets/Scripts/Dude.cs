458ae272f68a091231bdbcef404652de78d09d6e
fuzzball88 <teropelkonen88@gmail.com>
2018-11-08 14:34:17 +0200

    Created dude. Movement and look script started

---
+++ Assets/Scripts/Dude.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dude : MonoBehaviour {
+    public float playerSpeed = 5f;
+    public float rotationSpeed = 100f;
+
+    public Vector3 moveInput;
+    public Vector3 moveVelocity;
+
+    private Rigidbody myRigidbody;
+
+    private Camera mainCamera;
+
+    public AudioClip shoot;
+
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody>();
+        mainCamera = FindObjectOfType<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+        Debug.Log(Input.mousePosition);
+        LookAt();
+    }
+
+    private void FixedUpdate()
+    {
+        myRigidbody.velocity = moveVelocity;
+    }
+
+    void Movement()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * playerSpeed;
+        /*
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0,0,1) * playerSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, 0,-1) * playerSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * playerSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * playerSpeed * Time.deltaTime);
+        }
+        */
+    }
+
+    void LookAt()
+    {
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if(groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 PointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, PointToLook, Color.blue);
+
+            transform.LookAt(new Vector3(PointToLook.x, transform.position.y,PointToLook.z));
+        }
+    }
+}
