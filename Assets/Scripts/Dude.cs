7b78ca7dc628b7c4426569d5546ea9156482014f
fuzzball88 <teropelkonen88@gmail.com>
2018-11-09 13:02:21 +0200

    Projectile position and rotation fixed

--- Assets/Scripts/Dude.cs
+++ Assets/Scripts/Dude.cs
@@ -98,7 +98,7 @@ public class Dude : MonoBehaviour {
 
     void FireProjectile()
     {
-        GameObject shot = Instantiate(projectile, rightHand.transform.position, Quaternion.identity) as GameObject;
+        GameObject shot = Instantiate(projectile, rightHand.transform.position, gameObject.transform.rotation * projectile.transform.rotation) as GameObject;
         shot.GetComponent<Rigidbody>().AddForce(shot.transform.forward * projectileSpeed);
         //shot.GetComponent<Rigidbody>().velocity = new Vector3(0, projectileSpeed, 0);
         //AudioSource.PlayClipAtPoint(shoot, transform.position);
